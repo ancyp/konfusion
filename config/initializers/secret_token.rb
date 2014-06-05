632e41381d937fb61792f0af7563ec0a6ab50b57
ancy <ancyphilip1994@gmail.com>
2014-06-05 22:14:09 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Terminator::Application.config.secret_key_base = 'a4190b2e0a2a890ab45f5429bbae45fce40fe0ea505adb16906fbd250054b42940bc2238bad52bf994a70d76f2b216ed9bda58633776ecdfdd7d6d726346f17f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Terminator::Application.config.session_store :cookie_store, key: '_terminator_session'
